4a06dac751786d0ef0368649fa5157e322ff0c74
Natalya Kirpichnikova <kirpichnikova.nb@gmail.com>
2026-07-01 21:53:37 +0300

    Доработаны методы чтения файлов, обработки GET и POST запросов. Исправлен contacts.html для POST запроса.

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-# Импорт встроенной библиотеки для работы веб-сервера
+import os
 from http.server import BaseHTTPRequestHandler, HTTPServer
-import time
 
-# Для начала определим настройки запуска
-hostName = "localhost"  # Адрес для доступа по сети
-serverPort = 8080  # Порт для доступа по сети
+hostName = "localhost"
+serverPort = 8080
 
 
 class MyServer(BaseHTTPRequestHandler):
@@ -13,35 +11,68 @@ class MyServer(BaseHTTPRequestHandler):
         обработку входящих запросов от клиентов
     """
 
+    def __get_static_file(self, file_path):
+        """ Вспомогательный метод для чтения файлов из папки static """
+        # Проверяем, существует ли такой файл на диске
+        if os.path.exists(file_path) and os.path.isfile(file_path):
+            self.send_response(200)
+            # Задаем правильный тип
+            if file_path.endswith('.css'):
+                self.send_header('Content-type', 'text/css')
+            if file_path.endswith('.js'):
+                self.send_header('Content-type', 'text/javascript')
+            if file_path.endswith(('.png', '.jpg', '.jpeg', '.gif')):
+                self.send_header('Content-type', 'image/*')
+            self.end_headers()
+            # Читаем файлы из static в бинарном режиме
+            with open(file_path, 'rb') as f:
+                return f.read()
+        return None
+
+    def __get_contacts_page(self):
+        """ Метод чтения и возврата страницы Контакты """
+        with open('templates/contacts.html', 'r', encoding='utf-8') as f:
+            return f.read()
+
     def do_GET(self):
         """ Метод для обработки входящих GET-запросов """
-        self.send_response(200)  # Отправка кода ответа
-        self.send_header("Content-type", "application/json")  # Отправка типа данных, который будет передаваться
-        self.end_headers()  # Завершение формирования заголовков ответа
-        self.wfile.write(bytes("{'message': 'OK'}", "utf-8"))  # Тело ответа
+        # Обработка файлов из Static
+        if self.path.startswith('/static/'):
+            file_path = self.path.lstrip('/')
+            static_content = self.__get_static_file(file_path)
+            if static_content:
+                self.wfile.write(static_content)
+            else:
+                self.send_error(404, 'File not found in Static directory')
+            return
+        # Обработка HTML-страницы
+        elif self.path == "/" or self.path == '/contacts':
+            page_content = self.__get_contacts_page()
+            self.send_response(200)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(bytes(page_content, 'utf-8'))
+            return
+        else:
+            self.send_error(404, 'Page not found')
 
     def do_POST(self):
-        """ Метод для обработки входящих POST-запросов """
+        """ Метод для обработки входящих POST-запросов со страницы контактов"""
         content_length = int(self.headers['Content-Length'])
-        body = self.rfile.read(content_length)
+        body = self.rfile.read(content_length).decode('utf-8')
         print(body)
         self.send_response(200)
         self.end_headers()
 
 
 if __name__ == "__main__":
-    # Инициализация веб-сервера, который будет по заданным параметрам в сети
-    # принимать запросы и отправлять их на обработку специальному классу, который был описан выше
     webServer = HTTPServer((hostName, serverPort), MyServer)
     print("Server started http://%s:%s" % (hostName, serverPort))
 
     try:
-        # Cтарт веб-сервера в бесконечном цикле прослушивания входящих запросов
         webServer.serve_forever()
     except KeyboardInterrupt:
-        # Корректный способ остановить сервер в консоли через сочетание клавиш Ctrl + C
         pass
 
-    # Корректная остановка веб-сервера, чтобы он освободил адрес и порт в сети, которые занимал
     webServer.server_close()
     print("Server stopped.")
